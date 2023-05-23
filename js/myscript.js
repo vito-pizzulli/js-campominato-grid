a008ac09815871d03a32fab89dc3320e7ce091cf
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-05-23 17:14:14 +0200

    now func createDiv add two classes, code optimized

--- js/myscript.js
+++ js/myscript.js
@@ -10,46 +10,40 @@ playButton.addEventListener('click', function () {
     if (difficulty == 1) {
 
         for (let i = 1; i <= 100; i++) {
-            const gridCell = createDiv();
+            const gridCell = createDiv('grid-cell', 'difficulty-1-cell-width');
             gridCell.innerHTML = [i];
-            gridCell.classList.add('grid-cell');
             
             gridCell.addEventListener('click', function () {
                 gridCell.classList.add('clicked-cell');
                 console.log("Hai cliccato la cella n° " + [i])
             })
             
-            gridCell.classList.add('difficulty-1-cell-width');
             gridWrapper.appendChild(gridCell);
         }
     } else if (difficulty == 2) {
 
         for (let i = 1; i <= 81; i++) {
-            const gridCell = createDiv();
+            const gridCell = createDiv('grid-cell', 'difficulty-2-cell-width');
             gridCell.innerHTML = [i];
-            gridCell.classList.add('grid-cell');
             
             gridCell.addEventListener('click', function () {
                 gridCell.classList.add('clicked-cell');
                 console.log("Hai cliccato la cella n° " + [i])
             })
             
-            gridCell.classList.add('difficulty-2-cell-width');
             gridWrapper.appendChild(gridCell);
         }
     } else {
 
         for (let i = 1; i <= 49; i++) {
-            const gridCell = createDiv();
+            const gridCell = createDiv('grid-cell', 'difficulty-3-cell-width');
             gridCell.innerHTML = [i];
-            gridCell.classList.add('grid-cell');
             
             gridCell.addEventListener('click', function () {
                 gridCell.classList.add('clicked-cell');
                 console.log("Hai cliccato la cella n° " + [i])
             })
             
-            gridCell.classList.add('difficulty-3-cell-width');
             gridWrapper.appendChild(gridCell);
         }
     }
@@ -58,10 +52,13 @@ playButton.addEventListener('click', function () {
 /* FUNCTIONS */
 
 /**
- * This function creates an empty div in html.
- * @returns An empty div.
+ * This function creates an empty div in html with two css classes.
+ * @param {*} firstClass The first css class that the div will have.
+ * @param {*} secondClass The second css class that the div will have.
+ * @returns An empty div with two classes.
  */
-function createDiv () {
+function createDiv (firstClass, secondClass) {
     const div = document.createElement('div');
+    div.classList.add(firstClass, secondClass);
     return div;
 }
