9ae68eadf4904f12278b7f95ee80bc0431dd354c
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-05-23 15:41:18 +0200

    grid 100 cells dynamic creation almost completed

---
+++ js/myscript.js
@@ -0,0 +1,29 @@
+const playButton = document.querySelector('button');
+const gridWrapper = document.querySelector('div.grid-wrapper');
+
+playButton.addEventListener('click', function () {
+    gridWrapper.innerHTML = ' ';
+
+    for (let i = 1; i <= 100; i++) {
+        const gridCell = createDiv();
+        gridCell.innerHTML = [i];
+        gridCell.classList.add('grid-cell');
+        gridWrapper.appendChild(gridCell);
+    }
+
+    const gridCell = createDiv();
+    gridCell.classList.add('grid-cell');
+    gridWrapper.appendChild(gridCell);
+})
+
+
+/* FUNCTIONS */
+
+/**
+ * This function creates an empty div in html.
+ * @returns An empty div.
+ */
+function createDiv () {
+    const div = document.createElement('div');
+    return div;
+}
