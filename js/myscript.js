629ae8fd15e24e9b735ed0fa7539374d605e1508
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-05-23 15:46:30 +0200

    add black border class to grid wrapper wth button

--- js/myscript.js
+++ js/myscript.js
@@ -1,16 +1,18 @@
 const playButton = document.querySelector('button');
 const gridWrapper = document.querySelector('div.grid-wrapper');
+let gridCell;
 
 playButton.addEventListener('click', function () {
     gridWrapper.innerHTML = ' ';
 
     for (let i = 1; i <= 100; i++) {
-        const gridCell = createDiv();
+        gridCell = createDiv();
         gridCell.innerHTML = [i];
         gridCell.classList.add('grid-cell');
         gridWrapper.appendChild(gridCell);
     }
 
+    gridWrapper.classList.add('black-border')
     gridCell.classList.add('grid-cell');
     gridWrapper.appendChild(gridCell);
 })
