0bda3c7085bbaf0ea580d23689c16487cc1e4ca0
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-05-23 16:32:07 +0200

    bonus completed, added other difficulties

--- js/myscript.js
+++ js/myscript.js
@@ -1,21 +1,57 @@
 const playButton = document.querySelector('button');
 const gridWrapper = document.querySelector('div.grid-wrapper');
+const difficultySelect = document.querySelector('select');
 
 playButton.addEventListener('click', function () {
     gridWrapper.innerHTML = ' ';
     gridWrapper.classList.add('black-border')
+    difficulty = difficultySelect.value;
 
-    for (let i = 1; i <= 100; i++) {
-        const gridCell = createDiv();
-        gridCell.innerHTML = [i];
-        gridCell.classList.add('grid-cell');
+    if (difficulty == 1) {
 
-        gridCell.addEventListener('click', function () {
-            gridCell.classList.add('clicked-cell');
-            console.log("Hai cliccato la cella n° " + [i])
-        })
+        for (let i = 1; i <= 100; i++) {
+            const gridCell = createDiv();
+            gridCell.innerHTML = [i];
+            gridCell.classList.add('grid-cell');
+            
+            gridCell.addEventListener('click', function () {
+                gridCell.classList.add('clicked-cell');
+                console.log("Hai cliccato la cella n° " + [i])
+            })
+            
+            gridCell.classList.add('difficulty-1-cell-width');
+            gridWrapper.appendChild(gridCell);
+        }
+    } else if (difficulty == 2) {
 
-        gridWrapper.appendChild(gridCell);
+        for (let i = 1; i <= 81; i++) {
+            const gridCell = createDiv();
+            gridCell.innerHTML = [i];
+            gridCell.classList.add('grid-cell');
+            
+            gridCell.addEventListener('click', function () {
+                gridCell.classList.add('clicked-cell');
+                console.log("Hai cliccato la cella n° " + [i])
+            })
+            
+            gridCell.classList.add('difficulty-2-cell-width');
+            gridWrapper.appendChild(gridCell);
+        }
+    } else {
+
+        for (let i = 1; i <= 49; i++) {
+            const gridCell = createDiv();
+            gridCell.innerHTML = [i];
+            gridCell.classList.add('grid-cell');
+            
+            gridCell.addEventListener('click', function () {
+                gridCell.classList.add('clicked-cell');
+                console.log("Hai cliccato la cella n° " + [i])
+            })
+            
+            gridCell.classList.add('difficulty-3-cell-width');
+            gridWrapper.appendChild(gridCell);
+        }
     }
 })
 
