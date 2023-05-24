eb6f55950f7f7f259ac0bc9e3e28f12c17d72620
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-05-24 10:54:15 +0200

    add div click sound play to function divGenerator

--- js/myscript.js
+++ js/myscript.js
@@ -1,7 +1,8 @@
 const playButton = document.querySelector('button');
 const gridWrapper = document.querySelector('div.grid-wrapper');
 const difficultySelect = document.querySelector('select');
-const playButtonAudio = document.getElementById('audio');
+const playButtonAudio = document.getElementById('play-button-audio');
+const cellClickAudio = document.getElementById('cell-click-audio');
 
 playButton.addEventListener('click', function () {
     
@@ -12,13 +13,13 @@ playButton.addEventListener('click', function () {
     difficulty = difficultySelect.value;
 
     if (difficulty == 1) {
-        divGenerator (100, 'grid-cell', 'difficulty-1-cell-width', 'clicked-cell', gridWrapper);
+        divGenerator (100, 'grid-cell', 'difficulty-1-cell-width', 'clicked-cell', cellClickAudio, gridWrapper);
 
     } else if (difficulty == 2) {
-        divGenerator (81, 'grid-cell', 'difficulty-2-cell-width', 'clicked-cell', gridWrapper);
+        divGenerator (81, 'grid-cell', 'difficulty-2-cell-width', 'clicked-cell', cellClickAudio, gridWrapper);
 
     } else {
-        divGenerator (49, 'grid-cell', 'difficulty-3-cell-width', 'clicked-cell', gridWrapper);
+        divGenerator (49, 'grid-cell', 'difficulty-3-cell-width', 'clicked-cell', cellClickAudio, gridWrapper);
     }
 })
 
@@ -31,16 +32,19 @@ playButton.addEventListener('click', function () {
  * @param {*} firstClass The name of the first class that will be added to the created div.
  * @param {*} secondClass The name of the second class that will be added to the created div.
  * @param {*} clickClass The name of the class that will be added to the div only when clicking on him.
- * @param {*} divContainer The container inside which the divs will be created.
+ * * @param {*} clickSound The sound that will be played when clicking a div.
+ *  * @param {*} divContainer The container inside which the divs will be created.
  */
-function divGenerator (divNumber, firstClass, secondClass, clickClass, divContainer) {
+function divGenerator (divNumber, firstClass, secondClass, clickClass, clickSound, divContainer) {
 
     for (let i = 1; i <= divNumber; i++) {
             const cell = document.createElement('div');
             cell.innerHTML = [i];
             cell.classList.add(firstClass, secondClass);
             
-            cell.addEventListener('click', function () {
+            cell.addEventListener('click', function () { 
+                clickSound.load();
+                clickSound.play();
                 cell.classList.add(clickClass);
                 console.log("Hai cliccato la cella n° " + [i])
             })
